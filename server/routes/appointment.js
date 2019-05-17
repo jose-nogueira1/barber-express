0ded86c32cc2b22ba1ac09188ff515f7e14772d2
unknown <jose.nogueira.working@gmail.com>
2019-05-17 17:32:29 +0100

    new routes

--- server/routes/appointment.js
+++ server/routes/appointment.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const Appointment = require("../models/Appointment");
 const { isLoggedIn } = require("../middlewares");
+const BarberShop = require("../models/BarberShop")
 
 const router = express.Router();
 
-
 // Create an appointment
-router.post("/appointment", isLoggedIn, (req,res,next) => {
-  let { _barberShop, date, hourAndMinutes } = req.body
-  let _customer = req.user._id
+router.post("/appointment", isLoggedIn, (req, res, next) => {
+  let { _barberShop, date, hourAndMinutes } = req.body;
+  let _customer = req.user._id;
   Appointment.create({ _barberShop, date, hourAndMinutes, _customer })
     .then(appointment => {
       res.json({
@@ -16,69 +16,78 @@ router.post("/appointment", isLoggedIn, (req,res,next) => {
         appointment
       });
     })
-    .catch(err => next(err));   
+    .catch(err => next(err));
   // Bonus: make sure there is a free spot at that time
-})
+});
 
 // Route to get all appointments
-router.get("/appointment", (req,res,next) => {
+router.get("/appointment", (req, res, next) => {
   Appointment.find()
     .then(appointment => {
       res.json(appointment);
     })
-    .catch(err => next(err))
+    .catch(err => next(err));
 });
 
 // Route to get a Appointment by it's Id
-router.get("/appointment/:appointmentId", (req,res,next) => {
+router.get("/appointment/:appointmentId", (req, res, next) => {
   Appointment.findById(req.params.appointmentId)
     .then(appointment => {
-      if(appointment) {
-        res.json(appointment)
-      }
-      else {
+      if (appointment) {
+        res.json(appointment);
+      } else {
         next({
           status: 400,
-          message:'There is no Barber Shop with the id: '+ req.params.appointmentId
-        })
+          message:
+            "There is no Barber Shop with the id: " + req.params.appointmentId
+        });
       }
     })
-    .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 
 // Route to delete an appointment
-router.delete("/delete-appointment", isLoggedIn, (req,res,next) => {
-  Appointment.findOneAndRemove(req.params._id)
+router.delete("/appointments/:appointmentId", isLoggedIn, (req, res, next) => {
+  Appointment.findById(req.params.appointmentId)
     .then(appointment => {
-      res.json({
-        success: true,
-      })
+      if (req.user._id.equals(appointment._customer)) {
+        Appointment.findByIdAndDelete(appointment._id) 
+          .then( appointment => {
+            res.json({
+              success: true
+            });
+          })
+      } else {
+        next({
+          status: 403,
+          message: "User cannot delete other user appointment"
+        });
+      }
     })
-    .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 
 // Route to get all available appointment, on a specific date and a specific barber shop
-// Example input: GET /api/available-appointments (_baberShop="12345679abcdef", date="2019-05-16")
+// Example input: GET /api/available-times (_barberShop="12345679abcdef", date="2019-05-16")
 // Example output: [
 //  {numbersAndMinute: 540, status: "Available"},
 //  {numbersAndMinute: 585, status: "Unavailable"},
 //  {numbersAndMinute: 630, status: "Unavailable"},
 // ]
-router.get("/available-times", (req,res,next) => {
-  let { _barberShop, date } = req.body
-  Appointment.find()
-    .populate("_barberShop")
-    .populate("workingHours")
-    .then(appointment => {     
-          res.json({
-            success: true,
-            appointment
-          });
+router.get("/available-times/:barbershopId", (req, res, next) => {
+  let { _barberShop, date } = req.body;
+  let _id = req.params.barbershopId;
+  BarberShop.findById({_id})
+    .then(barbershop => {
+      Appointment.find({_barberShop:_id})
+      .then(appointments => {
+        res.json({
+          success: true,
+          workingHours
+        })
+      })
     })
     .catch(err => next(err))
-  // TODO: continue 
-
-})
-
-
+});
+  
 module.exports = router;
--- server/routes/barberShops.js
+++ server/routes/barberShops.js
@@ -1,50 +1,24 @@
 const express = require("express");
-const BarberShop = require("../models/barberShop");
+const BarberShop = require("../models/BarberShop");
 const { isLoggedIn } = require("../middlewares");
 const Cloudinary = require("../configs/cloudinary");
 
 const router = express.Router();
 
 // Route to create a BarberShop
-router.post("/barbershop", Cloudinary.single("picture"), isLoggedIn, (req, res, next) => {
-  let {
-    name,
-    gender,
-    streetAddress,
-    city,
-    country,
-    lat,
-    lng,
-    workingHourMonBegin,
-    workingHourMonEnd,
-    workingHourTueBegin,
-    workingHourTueEnd,
-    workingHourWedBegin,
-    workingHourWedEnd,
-    workingHourThuBegin,
-    workingHourThuEnd,
-    workingHourFriBegin,
-    workingHourFriEnd,
-    workingHourSatBegin,
-    workingHourSatEnd,
-    workingHourSunBegin,
-    workingHourSunEnd
-  } = req.body;
-  let _owner = req.user._id;
-  let logo = req.file.url;
-  BarberShop.create({
-    _owner,
-    name,
-    gender,
-    address: {
+router.post(
+  "/barbershop",
+  Cloudinary.single("picture"),
+  isLoggedIn,
+  (req, res, next) => {
+    let {
+      name,
+      gender,
       streetAddress,
       city,
       country,
-      location: {
-        coordinates: [ lat, lng ]
-      },
-    },
-    workingHours:{
+      lat,
+      lng,
       workingHourMonBegin,
       workingHourMonEnd,
       workingHourTueBegin,
@@ -59,53 +33,93 @@ router.post("/barbershop", Cloudinary.single("picture"), isLoggedIn, (req, res, 
       workingHourSatEnd,
       workingHourSunBegin,
       workingHourSunEnd
-    },
-    logo
-  })
-    .then(barbershop => {
-      res.json({
-        success: true,
-        barbershop
-      });
+    } = req.body;
+    let _owner = req.user._id;
+    let logo = req.file && req.file.url;
+    BarberShop.create({
+      _owner,
+      name,
+      gender,
+      address: {
+        streetAddress,
+        city,
+        country,
+        location: {
+          coordinates: [lat, lng]
+        }
+      },
+      workingHours: {
+        workingHourMonBegin,
+        workingHourMonEnd,
+        workingHourTueBegin,
+        workingHourTueEnd,
+        workingHourWedBegin,
+        workingHourWedEnd,
+        workingHourThuBegin,
+        workingHourThuEnd,
+        workingHourFriBegin,
+        workingHourFriEnd,
+        workingHourSatBegin,
+        workingHourSatEnd,
+        workingHourSunBegin,
+        workingHourSunEnd
+      },
+      logo
     })
-    .catch(err => next(err));
-});
+      .then(barbershop => {
+        res.json({
+          success: true,
+          barbershop
+        });
+      })
+      .catch(err => next(err));
+  }
+);
 
 // Route to get all Barber Shops
-router.get("/barbershop", (req,res,next) => {
+router.get("/barbershop", (req, res, next) => {
   BarberShop.find()
     .then(barbershop => {
       res.json(barbershop);
     })
-    .catch(err => next(err))
+    .catch(err => next(err));
 });
 
 // Route to get a Barber Shop by it's Id
-router.get("/barbershop/:barbershopId", (req,res,next) => {
+router.get("/barbershop/:barbershopId", (req, res, next) => {
   BarberShop.findById(req.params.barbershopId)
     .then(barbershop => {
-      if(barbershop) {
-        res.json(barbershop)
-      }
-      else {
+      if (barbershop) {
+        res.json(barbershop);
+      } else {
         next({
           status: 400,
-          message:'There is no Barber Shop with the id: '+ req.params.barbershopId
-        })
+          message:
+            "There is no Barber Shop with the id: " + req.params.barbershopId
+        });
       }
     })
-    .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 
 //Route to delete a Barber Shop
-router.delete("/delete-barbershop", isLoggedIn, (req,res,next) => {
-  BarberShop.findOneAndRemove(req.params._id)
+router.delete("/barbershop/:barbershopId", isLoggedIn, (req, res, next) => {
+  BarberShop.findById(req.params.barbershopId)
     .then(barbershop => {
-      res.json({
-        success: true,
-      })
+      if (req.user._id.equals(barbershop._owner)) {
+        BarberShop.findOneAndRemove(req.params._id).then(barbershop => {
+          res.json({
+            success: true
+          });
+        });
+      } else {
+        next({
+          status: 403,
+          message: "User cannot this Barber Shop"
+        });
+      }
     })
-    .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 
 module.exports = router;
